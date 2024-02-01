b4686f471d6a8599c303c79c6e282bbbb59970c6
n-haruhi <hr23nakanishi@gmail.com>
2024-02-01 14:38:28 +0000

    create homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  #URL「top」にアクセスしたときにhomesコントローラのtopアクションが呼び出される
+  get 'top' => 'homes#top'
+  root 'homes#top'
+  get 'about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
